3f078c0804fdac8c8b1e9f17e22d8e86cd2f3580
Huy Tran <huy454957@gmail.com>
2024-03-07 01:35:25 +0100

    PHP

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,29 +1,38 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+require 'path/to/PHPMailer/PHPMailer.php'; // Stellen Sie sicher, dass Sie den richtigen Pfad zu PHPMailer angeben
 
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-    $email_from = 'info@easytutorialspro.com';
+$email_from = 'info@mcänderungsschneiderei-berlin.de';
+$email_subject = "New Form Submission";
+$email_body =   "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
 
-    $email_subject = "New Form Submission";
-    
-    $email_body =   "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                    "Subject: $subject.\n".
-                    "User Message: $message.\n";
+$to = "huy454957@gmail.com";
 
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->Host = 'smtp.ionos.com'; // IONOS SMTP-Server
+$mail->SMTPAuth = true;
+$mail->Username = 'info@mcänderungsschneiderei-berlin.de'; // Ihr IONOS-Benutzername
+$mail->Password = 'Huyonweed12345!'; // Ihr IONOS-Passwort
+$mail->SMTPSecure = 'tls'; // Verwenden Sie 'tls' oder 'ssl' je nach IONOS-Einstellungen
+$mail->Port = 587; // Port für TLS
 
-    $to = "avinash6252@gmail.com";
+$mail->setFrom($email_from);
+$mail->addAddress($to);
+$mail->isHTML(true); // Setzen Sie auf true, wenn Sie HTML-E-Mails senden möchten
+$mail->Subject = $email_subject;
+$mail->Body = $email_body;
 
-    $headers = "From: $email_from \r\n";
-
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html");
-
-
+if ($mail->send()) {
+    header("Location: contact.html"); // Erfolgreich versendet
+} else {
+    echo "E-Mail konnte nicht gesendet werden. Fehler: " . $mail->ErrorInfo;
+}
 ?>
