00881e47b780be449f4552c05f7bafa44670e1f5
Huy Tran <huy454957@gmail.com>
2024-03-07 02:28:15 +0100

    PHP

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,39 +4,47 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php'; // Stellen Sie sicher, dass der Pfad korrekt ist
 
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+try {
+     // Instanz der PHPMailer-Klasse erstellen
+    $mail = new PHPMailer($debug);
 
-$email_from = 'info@mcänderungsschneiderei-berlin.de';
-$email_subject = "New Form Submission";
-$email_body =   "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "Subject: $subject.\n".
-                "User Message: $message.\n";
+    if ($debug) {
+                // gibt einen ausführlichen log aus
+            $mail->SMTPDebug = PHPMailer\PHPMailer\SMTP::DEBUG_SERVER;
+    }
 
-$to = "huy454957@gmail.com";
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$mail = new PHPMailer(true);
-$mail->isSMTP();
-$mail->Host = 'smtp.ionos.de'; // IONOS SMTP-Server
-$mail->SMTPAuth = true;
-$mail->Username = 'info@mcänderungsschneiderei-berlin.de'; // Ihr IONOS-Benutzername
-$mail->Password = 'Huyonweed12345!'; // Ihr IONOS-Passwort
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-$mail->Port = 587; // Port für TLS
+    $email_from = 'info@mcänderungsschneiderei-berlin.de';
+    $email_subject = "New Form Submission";
+    $email_body =   "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                    "User Message: $message.\n";
 
-$mail->setFrom($email_from);
-$mail->addAddress($to);
-$mail->isHTML(true); // Setzen Sie auf true, wenn Sie HTML-E-Mails senden möchten
-$mail->Subject = $email_subject;
-$mail->Body = $email_body;
+    $to = "huy454957@gmail.com";
 
-if ($mail->send()) {
-    header("Location: contact.html"); // Erfolgreich versendet
-} else {
-    echo "E-Mail konnte nicht gesendet werden. Fehler: " . $mail->ErrorInfo;
-}
-?>
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.ionos.de'; // IONOS SMTP-Server
+    $mail->SMTPAuth = true;
+    $mail->Username = 'info@mcänderungsschneiderei-berlin.de'; // Ihr IONOS-Benutzername
+    $mail->Password = 'Huyonweed12345!'; // Ihr IONOS-Passwort
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 587; // Port für TLS
+
+    $mail->setFrom($email_from);
+    $mail->addAddress($to);
+    $mail->isHTML(true); // Setzen Sie auf true, wenn Sie HTML-E-Mails senden möchten
+    $mail->Subject = $email_subject;
+    $mail->Body = $email_body;
+
+    $mail->send();
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: ".$mail->ErrorInfo;
+}   
